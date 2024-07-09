ddb3774b667c2ab99d7c931b7edcf750990a0a94
yshujie <yshujie@163.com>
2024-07-09 12:29:00 +0800

    Fix change mysql logging driver

--- internal/repository/mysql/logging_driver.go
+++ internal/repository/mysql/logging_driver.go
@@ -1,82 +1,75 @@
 package mysql
 
 import (
-	"database/sql"
-	"database/sql/driver"
-	"time"
+    "database/sql"
+    "database/sql/driver"
+    "log"
+    "time"
 
-	"user-profile-converter/pkg/logging"
-	_ "github.com/go-sql-driver/mysql"
+    "github.com/go-sql-driver/mysql"
 )
 
-// loggingDriver is a wrapper around a driver.Driver that logs queries.
 type loggingDriver struct {
-	driver.Driver
+    driver.Driver
 }
 
-// Open implements the driver.Driver interface.
 type loggingConn struct {
-	driver.Conn
+    driver.Conn
 }
 
-// Prepare implements the driver.Conn interface.
 type loggingStmt struct {
-	driver.Stmt
-	query string 
+    driver.Stmt
+    query string
 }
 
-// Exec implements the driver.Stmt interface.
 type loggingTx struct {
-	driver.Tx
+    driver.Tx
 }
 
 func init() {
-	sql.Register("mysql-logging", &loggingDriver{Driver: mysql.MySQLDriver})
+    sql.Register("mysql-logging", &loggingDriver{Driver: &mysql.MySQLDriver{}})
 }
 
-
-func (d *loggingDriver) Open(name string) (driver.Stmt, error) {
-	conn, err := d.Driver.Open(name)
-	if err != nil {
-		return nil, err
-	}
-
-	return &loggingConn{Conn: conn}, nil
+func (d *loggingDriver) Open(name string) (driver.Conn, error) {
+    conn, err := d.Driver.Open(name)
+    if err != nil {
+        return nil, err
+    }
+    return &loggingConn{Conn: conn}, nil
 }
-
 
 func (c *loggingConn) Prepare(query string) (driver.Stmt, error) {
-	stmt, err := c.Conn.Prepare(query)
-	if err != nil {
-		return nil, err
-	}
-
-	return &loggingStmt{Stmt: stmt, query: query}, nil
+    stmt, err := c.Conn.Prepare(query)
+    if err != nil {
+        return nil, err
+    }
+    return &loggingStmt{Stmt: stmt, query: query}, nil
 }
 
 func (c *loggingConn) Begin() (driver.Tx, error) {
-	tx, err := c.conn.Begin()
-	if err != nil {
-		return nil, err
-	}
-
-	return &loggingTx{Tx: tx}, nil
+    tx, err := c.Conn.Begin()
+    if err != nil {
+        return nil, err
+    }
+    return &loggingTx{Tx: tx}, nil
 }
 
 func (s *loggingStmt) Exec(args []driver.Value) (driver.Result, error) {
-	start := time.Now()
-	result, err := s.Stmt.Exec(args)
-	elapsed := time.Since(start)
-
-	logQuery("Exec", s.query, elapsed, err)
-	return result, err
+    start := time.Now()
+    result, err := s.Stmt.Exec(args)
+    elapsed := time.Since(start)
+    logQuery(s.query, args, elapsed, err)
+    return result, err
 }
 
 func (s *loggingStmt) Query(args []driver.Value) (driver.Rows, error) {
-	start := time.Now()
-	rows, err := s.Stmt.Query(args)
-	elapsed := time.Since(start)
+    start := time.Now()
+    rows, err := s.Stmt.Query(args)
+    elapsed := time.Since(start)
+    logQuery(s.query, args, elapsed, err)
+    return rows, err
+}
 
-	logQuery("Query", s.query, elapsed, err)
-	return rows, err
+func logQuery(query string, args []driver.Value, elapsed time.Duration, err error) {
+    log.Printf("query: %s, args: %v, elapsed: %v, error: %v", query, args, elapsed, err)
 }
