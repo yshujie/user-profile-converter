106b0fff81391f95a6f8c0e3cc55d2317cb43160
yshujie <yshujie@163.com>
2024-07-30 09:41:13 +0800

    Fix remove database connection code

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,16 +2,7 @@ package main
 
 import (
 	"log"
-	"os"
-	"user-profile-converter/innternal/repository/mongodb"
-	"user-profile-converter/innternal/repository/mysql"
-	"user-profile-converter/internal/service"
-	"user-profile-converter/pkg/mysql"
-	"user-profile-converter/pkg/mongodb"
-
 	"github.com/joho/godotenv"
-	"go.mongidb.org/mongo-driver/bson"
-	"context"
 )
 
 func main() {
@@ -19,18 +10,5 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	mysqlDB,err := mysql.Connect()
-	if err != nil {
-		log.Fatal("Faild to connect to MySQL, err: ", err)
-	}
-	defer mysqlDB.Close()
-
-	mongoClient, err := mongodb.Connect()
-	if err != nil {
-		log.Fatal("Failed to connect to MongoDB, err: ", err)
-	}
-	defer mongoClient.Disconnect(context.Background())
-
-	log.Println("Connected to MySQL and MongoDB")
+		
 }
