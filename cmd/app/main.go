e51e198ecbb685b6e687e7d474b691c4062ccdc0
yshujie <yshujie@163.com>
2024-08-06 17:59:46 +0800

    Feat connected mysql by gorm

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -10,5 +15,27 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-		
+
+	initMySQLClient()
+}
+
+var DB *gorm.DB
+
+func initMySQLClient() {
+	// Initialize MySQL client
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?chartset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_NAME"),
+	)
+
+	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	fmt.Println("Connected to MySQL database, client: ", DB)
 }
